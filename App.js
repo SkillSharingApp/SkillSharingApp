ace25859a8dc0dfb99f1eb92c63d0b697301e912
Sam Arnold <sama@Sams-MacBook-Pro.local>
2021-10-28 21:24:13 -0400

    HomeView demo done

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import views from './views/index';
 
 const {
