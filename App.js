2868f3d95e197a9704f38e15ab1ef754ddf37308
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-11-02 00:59:11 -0400

    Basic video set up

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {View, Text, Button} from 'react-native';
+
+import {
+  View,
+  Text,
+  Button,
+  StatusBar,
+  StyleSheet
+} from 'react-native';
 
 import {
   RTCPeerConnection,
@@ -12,26 +19,66 @@ import {
   mediaDevices,
   registerGlobals
 } from 'react-native-webrtc';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
+const props = {
+
+};
+
+const App = (props) => {
+  // set instructorStream and studentStream states and handlers
+  const [instructorStream, setInstructorStream] = useState(null);
+  const [studentStream, setStudentStream] = useState(null);
+
+  // functions to start and stop instructor stream
+  const startInstructorStream = async () => {
+    if (!instructorStream) {
+      try {
+        let mediaStream = await mediaDevices.getUserMedia({ video: true, audio: true });
+        setInstructorStream(mediaStream);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+  const stopInstructorStream = () => {
+    if (instructorStream) {
+      instructorStream.release();
+      setInstructorStream(null);
+    }
+  };
+
+
   return (
-  <View style={{ flex: 1, justifyContent: "space-evenly", alignItems: "center" }}>
+  // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  <View style={styles.body}>
+    <StatusBar barStyle="dark-content" />
+    {instructorStream && <RTCView streamURL = {instructorStream.toURL()} style={styles.stream} />}
     <View>
-      <Text>webRTC</Text>
+      <Button title = "Start" onPress = {startInstructorStream} />
+      <Button title = "Stop" onPress = {stopInstructorStream} /> 
     </View>
-    <RTCView />
-    <View>
-      <Text>view2</Text>
-    </View>
-    <View>
-      <Text>view3</Text>
-    </View>
-    <Button
-      title="Button1" />
-    <Button
-      title="Button2" />
-   </View>
+  </View>
   );
 };
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: Colors.pink,
+    ...StyleSheet.absoluteFill
+  },
+  stream: {
+    flex: 1,
+    height: 100,
+  },
+  footer: {
+    backgroundColor: Colors.lighter,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
+  }
+});
 
 export default App;
